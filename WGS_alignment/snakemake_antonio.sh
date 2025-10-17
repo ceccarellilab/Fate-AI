10ea26a5cfdcb470bf22067480be432269bdbe00
AntonioDeFalco <antoniodf92na@gmail.com>
2025-10-17 18:33:26 +0200

    commit

--- WGS_alignment/snakemake_antonio.sh
+++
@@ -1,5 +0,0 @@
-#Snakemake Slurm 9.12.0
-mamba create -c conda-forge -c bioconda -c nodefaults -n snakemake_last snakemake snakemake-executor-plugin-slurm
-conda activate snakemake_last
-/scratch/adefalco/miniconda3/envs/snakemake_last/bin/snakemake
-
